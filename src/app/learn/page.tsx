a7f3dcae63eee8879107beca6a3a90fdbd56f7eb
yosh-25 <yoshi.okada25@gmail.com>
2024-03-16 01:05:39 +0900

    検索ワードの最初の記事を表示するよう実装

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import {
   Box,
@@ -10,33 +10,29 @@ import {
   Input,
 } from "@mui/material";
 
-
-
 export default function Learn() {
+  const [textFromAPI, setTextFromAPI] = useState("");
+  const [searchWord, setSearchWord] = useState("");
 
-  const [ textFromAPI, setTextFromAPI] = useState('');
-  const [ searchWord, setSearchWord] = useState('');
-
-  useEffect(() => {
-    fetch(`/api/proxy/w/api.php?action=query&format=json&prop=extracts&titles=${searchWord}&formatversion=2&exchars=1000&explaintext`)
-      .then(response => {
-        console.log('Response status:', response.status); // レスポンスのステータスを確認
+  const handleSearch = () => {
+    fetch(
+      `/api/proxy/w/api.php?action=query&format=json&prop=extracts&titles=${searchWord}&formatversion=2&exchars=1000&explaintext`
+    )
+      .then((response) => {
         return response.json();
       })
-      .then(data => {
-        console.log('Data:', data); // 受信したデータをログに出力
+      .then((data) => {
+        console.log("Data:", data); // 受信したデータをログに出力
         // APIのレスポンスから必要な箇所にアクセス。テキストを抽出。
         const page = data.query.pages[0];
         if (page && page.extract) {
           setTextFromAPI(page.extract);
         }
       })
-      .catch(error => {
-        console.error('Fetch error:', error); // エラーがあれば出力
+      .catch((error) => {
+        console.error("Fetch error:", error); // エラーがあれば出力
       });
-      
-
-  }, []);
+  };
 
   return (
     <Box display="flex" justifyContent="center" alignItems="center">
@@ -57,9 +53,15 @@ export default function Learn() {
         >
           <Box mb={2}>
             <Typography>検索</Typography>
-            <Input type="text" value={searchWord} onChange={(e)=> setSearchWord(e.target.value)} placeholder="検索ワードを入力" />
+            <Input
+              type="text"
+              value={searchWord}
+              onChange={(e) => setSearchWord(e.target.value)}
+              placeholder="検索ワードを入力"
+            />
           </Box>
         </Box>
+        <Button onClick={handleSearch}>Press</Button>
         <Box
           width="50%"
           border={2}
@@ -70,7 +72,6 @@ export default function Learn() {
           {/* 検索したワードのテキストを表示 */}
           <Typography>{textFromAPI} </Typography>
         </Box>
-        <Button>Press</Button>
       </Box>
     </Box>
   );
