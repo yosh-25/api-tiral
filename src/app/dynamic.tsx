8d3910b772edeaad7655b4e0319b43759e6965b5
yosh-25 <yoshi.okada25@gmail.com>
2024-06-01 11:40:17 +0900

    dynamic削除

--- src/app/dynamic.tsx
+++
@@ -1,9 +0,0 @@
-// import dynamic from 'next/dynamic';
-
-// export default dynamic(
-//   async () => {
-//     const module = await import('@/app/mypage/searchResults/page');
-//     return module.Component;
-//   },
-//   { ssr: false }
-// );
