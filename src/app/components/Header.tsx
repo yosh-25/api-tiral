99311e44e75941dbb94e6804d9c69dc583e5a4c8
yosh-25 <yoshi.okada25@gmail.com>
2024-06-08 21:30:12 +0900

    signoutコンポーネント化

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,6 +3,7 @@ import { useAuth } from '../../../context/AuthContext'
 import { getAuth, signOut } from "firebase/auth";
 import ButtonToAddMemo from './elements/buttons/buttonToAddMemo';
 import MainButton from './elements/buttons/mainButton';
+import Signout from './Signout';
 
 const Header = () => {
   const { currentUser }:any = useAuth();
@@ -10,14 +11,7 @@ const Header = () => {
 
   return (
     <Box style={{ padding: "1rem 0" }} >
-      { currentUser ? (
-        // useAuth()で取得した現在ログインしているユーザーのメールアドレスをcurrentUser.emailで表示
-        <Box suppressHydrationWarning={true}>
-          <MainButton style={{ paddingBottom: "1rem" }}>ログアウト </MainButton>
-        </Box>
-      ):(
-        <div suppressHydrationWarning={true}>会員登録/ログインはこちら</div>
-      )}
+      <Signout/>
     </Box>
   );
 }
--- src/app/components/Signout.tsx
+++ src/app/components/Signout.tsx
@@ -1,146 +1,41 @@
 "use client";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+import { Box, Typography } from '@mui/material';
 import { signInWithEmailAndPassword, getAuth, signOut } from "firebase/auth";
 import { useAuth } from "../../../context/AuthContext";
 import {} from "firebase/auth";
-import { FirebaseError } from "@firebase/util";
-import { auth } from "../../../lib/firebase";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import Header from "../components/Header";
-import MainButton from "../components/elements/buttons/mainButton";
+import ButtonToAddMemo from './elements/buttons/buttonToAddMemo';
+import MainButton from './elements/buttons/mainButton';
+import Link from "next/link";
 
-const login = () => {
+  const Signout = () => {
   const router = useRouter();
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-
-  const { currentUser } = useAuth();
-
-  const doSignin = async (event: React.FormEvent) => {
-    event.preventDefault();
-
-    try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      // サインアップ成功時の処理
-      console.log("User signed up:", userCredential);
-      setEmail("");
-      setPassword("");
-    } catch (e) {
-      if (e instanceof FirebaseError) {
-        console.log(e);
-      }
-    }
-  };
-
-  // ログアウトの処理を追記
-  const doLogout = () => {
+  const { currentUser }:any = useAuth();
     const auth = getAuth();
-
+    const doLogout=() => {
     signOut(auth)
       .then(() => {
-        // ログアウトされたことをわかりやすくするためのアラート
-        alert("ログアウト完了！");
+        console.log("ログアウト完了！");
+        router.push("/signin");
       })
       .catch((error) => {
         console.log(error);
-      });
-  };
-
-  return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <Header />
-        <Box component="form" noValidate onSubmit={doSignin} sx={{ mt: 3 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                type="email"
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="new-password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
-              />
-            </Grid>
-          </Grid>
-          <div>
-            <Button
-              onClick={() => {
-                doLogout();
-              }}
-            >
-              ログアウト
-            </Button>
-          </div>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign in
-          </Button>
-          <MainButton>Test</MainButton>
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
+      });  
+    };
+  return (    
+           <>
+             { currentUser ? (
+              <Box suppressHydrationWarning={true}>
+                <MainButton onClick={doLogout}>ログアウト </MainButton>
+              </Box>
+            ):(
+              <Link href='/src/app/signin/page.tsx'>
+              <Typography suppressHydrationWarning={true}>会員登録/ログインはこちら</Typography>
               </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-    </Container>
+            )}
+         </>
   );
 };
 
-export default login;
+export default Signout;
