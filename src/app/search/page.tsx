4d11a03afd1a82357cc27ebb7810c811b9d3067f
yosh-25 <yoshi.okada25@gmail.com>
2024-06-30 02:18:15 +0900

    ログイン確認箇所にuseEffec追加

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../../../context/AuthContext";
 import { Box, Typography } from "@mui/material";
@@ -8,50 +8,58 @@ import SearchIconAndFunction from "../components/SearchIconAndFunction";
 const Search = () => {
   const router = useRouter();
   const { currentUser } = useAuth();
+  const [loading, setLoading] = React.useState(true);
 
-  // ログインしていなければサインインページへ
-  if (!currentUser) router.replace("/signin");
+  useEffect(() => {
+    if (!currentUser) {
+      router.replace("/signin");
+    } else {
+      setLoading(false);
+    }
+  }, [currentUser, router]);
+
+  if (loading) {
+    return <Box>Loading...</Box>;
+  }
 
   return (
-    <>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          mt: {
-            xs: 15,
-            md: 30,
-          },
-        }}
-      >
-        <Box sx={{ width: "100%", textAlign: "center", mb: "3em" }}>
-          <Typography
-            variant="h3"
-            sx={{
-              fontSize: {
-                xs: "2em",
-                md: "2.5em",
-                lg: "3em",
-              },
-            }}
-          >
-            Memotube
-          </Typography>
-        </Box>
-        <Box
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        mt: {
+          xs: 15,
+          md: 30,
+        },
+      }}
+    >
+      <Box sx={{ width: "100%", textAlign: "center", mb: "3em" }}>
+        <Typography
+          variant="h3"
           sx={{
-            width: "70%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
+            fontSize: {
+              xs: "2em",
+              md: "2.5em",
+              lg: "3em",
+            },
           }}
         >
-          <SearchIconAndFunction />
-        </Box>
+          Memotube
+        </Typography>
       </Box>
-    </>
+      <Box
+        sx={{
+          width: "70%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <SearchIconAndFunction />
+      </Box>
+    </Box>
   );
 };
 
