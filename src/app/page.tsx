930c653690a65fa997a921ace4c2acda09c32ebc
yosh-25 <yoshi.okada25@gmail.com>
2024-07-02 21:15:28 +0900

    デザイン編集中

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,15 @@ import { useAuth } from "../../context/AuthContext";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "@/theme";
 import Head from "next/head";
+import Footer from "./components/Footer";
 
 export default function Home() {
   const router = useRouter();
   const [YTPlayer, setYTPlayer] = useState<YT.Player>();
   const { currentUser } = useAuth();
-  const isXs = useMediaQuery(theme.breakpoints.down('sm'));
-  const isMd = useMediaQuery(theme.breakpoints.between('sm', 'md'));
-  const isLg = useMediaQuery(theme.breakpoints.up('md'));
+  const isXs = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMd = useMediaQuery(theme.breakpoints.between("sm", "md"));
+  const isLg = useMediaQuery(theme.breakpoints.up("md"));
 
   // 前回ログインしたままの場合メインページへ遷移
   useEffect(() => {
@@ -26,13 +27,13 @@ export default function Home() {
 
   const getYouTubeOpts = () => {
     if (isXs) {
-      return { width: '375px', height: '220px' };
+      return { width: "375px", height: "220px" };
     } else if (isMd) {
-      return { width: '800px', height: '450px' };
+      return { width: "800px", height: "450px" };
     } else if (isLg) {
-      return { width: '1100px', height: '620px' };
+      return { width: "1000px", height: "600px" };
     } else {
-      return { width: '100%', height: 'auto' };
+      return { width: "100%", height: "auto" };
     }
   };
 
@@ -44,51 +45,73 @@ export default function Home() {
 
   return (
     <html lang="ja">
-    <Head>
-      <title>サインイン - Memotube</title>
-      <meta name="description" content="Memotubeへのサインインページです。" />
-    </Head>
-    <body>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-          marginTop: '4em'
-        }}
-      >
-        <Stack alignItems="center" gap="2rem">
-          
-          <Typography variant="h1" fontSize="3rem" textAlign='center' fontWeight="750">
-            Memotube
-          </Typography>
+      <Head>
+        <title>サインイン - Memotube</title>
+        <meta name="description" content="Memotubeへのサインインページです。" />
+      </Head>
+      <body>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              textAlign: "center",
+              marginTop: "4em",
+              minHeight: "100vh",  
+            }}
+          >
+            <Stack alignItems="center" gap="2rem" sx={{mb:{ lg:'3em'}}} >
+              <Typography
+                variant="h1"
+                fontSize="3rem"
+                textAlign="center"
+                fontWeight="750"
+              >
+                Memotube
+              </Typography>
 
-          <Box width='70%'>
-          <Typography variant="h2" fontWeight="500" mb='1em' sx={{fontSize:{xs:'1.3em', md:"2em"}}} >
-            Youtube動画を検索して、<br/>視聴しながら好きな箇所でメモが取れます。
-          </Typography>
+              <Box width="70%">
+                <Typography
+                  variant="h2"
+                  fontWeight="500"
+                  mb="1em"
+                  sx={{ fontSize: { xs: "1.3em", md: "1.8em" } }}
+                >
+                  Youtube動画を検索して、
+                  <br />
+                  視聴しながら好きな箇所でメモが取れます。
+                </Typography>
 
-          <Typography fontWeight="500" sx={{fontSize:{xs:'1.3em', md:"2em"}}}>
-            動画をクリックして、このアプリの使い方を見てみましょう💡
-          </Typography>
+                <Typography
+                  fontWeight="500"
+                  sx={{ fontSize: { xs: "1.3em", md: "1.8em" } }}
+                >
+                  動画をクリックして、
+                  <br />
+                  このアプリの使い方を見てみましょう💡
+                </Typography>
+              </Box>
+
+              <YouTube
+                videoId="CtMVk75abXg"
+                opts={opts}
+                onReady={makeYTPlayer}
+              />
+              
+              <Link href="/signup">
+                <MainButton>初めての方はこちら</MainButton>
+              </Link>
+
+              <Link href="/signin">
+                <MainButton sx={{ width: "9rem" }}>ログイン</MainButton>
+              </Link>
+            </Stack>
           </Box>
-          
-          <YouTube videoId="CtMVk75abXg" opts={opts} onReady={makeYTPlayer} />
-
-          <Link href="/signup">
-            <MainButton>初めての方はこちら</MainButton>
-          </Link>
-
-          <Link href="/signin">
-            <MainButton sx={{ width: "9rem" }}>ログイン</MainButton>
-          </Link>
-        </Stack>
-      </Box>
-      </ThemeProvider>
+          <Footer/>
+        </ThemeProvider>
       </body>
     </html>
   );
