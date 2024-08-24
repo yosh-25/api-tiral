bb700656103f4317886ea2857b21c5866f5b018d
yosh-25 <yoshi.okada25@gmail.com>
2024-08-24 23:44:32 +0900

    update demo account

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -145,9 +145,9 @@ const Signin = () => {
               </Box>
             </Grid>
             <Grid item sx={{ mt: "1rem" }}>
-              Demo Email Addres: 2xa5ri7llkc_@gmail.com
+              Demo Email Addres: demo12345@gmail.com
               <br />
-              Demo Password: z1zHAVL1
+              Demo Password: Password1
             </Grid>
           </Grid>
         </Box>
